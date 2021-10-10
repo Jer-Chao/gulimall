58a072af019513184d0a44c3f81c0306e8d04452
Jer-Chao <ccdj@qq.com>
2021-10-10 11:26:03 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/mrcc/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mrcc.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
